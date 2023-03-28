f51bfb8331bb2f55792f86dea5351e6d548862ba
Aleksandar Matovic <am20190138@student.fon.bg.ac.rs>
2023-03-29 01:49:13 +0200

    Kreirana konekcija sa bazom

---
+++ functions/db.php
@@ -0,0 +1,20 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', '', 'social_network');
+
+function escape($string){
+    global $con;
+    return mysqli_real_escape_string($con, $string);
+} // pomoci ce kada budemo izvrsavali upite u bazi podataka
+
+function query($query){
+    global $con;
+    return mysqli_query($con, $query); //uzima query iz argumenta ubacuje ga dole u return 
+}
+
+function confirm($result){
+    global $con;
+    if(!$result){ // ako postoji greska on ce da kaze query failed zaustavi 
+        die("Query Failed" . mysqli_error($con));
+    }
+}
