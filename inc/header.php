bf395a170eba85ac9ae1b98663bd5c1bcb37afe3
Aleksandar Matovic <am20190138@student.fon.bg.ac.rs>
2023-03-29 02:23:24 +0200

    Kreirana glavna strana, header, footer i css za njih

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<?php
+
+include "functions/init.php" // includovacu header na ostalim stranicama, sa njim ce biti includovan i init.php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Anime zajednica</title>
+    
+</head>
+<body>
+    
+
+<div class="container"> <?php// kontejner za sajt ?>
+
+    <ul>
+        <li><a href="index.php"> Home </a></li>
+        <li><a href="login.php"> Login </a></li>
+        <li><a href="register.php"> Register </a></li>
+</ul>
